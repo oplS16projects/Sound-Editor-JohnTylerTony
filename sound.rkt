02ddcbe97aa20506620ec5c00d4329ddddc08f23
tylerbezuka <Tyler_Bezuka@student.uml.edu>
2016-04-16 22:11:46 -0400

    updated with initial sound definitions

--- sound.rkt
+++ sound.rkt
@@ -10,5 +10,15 @@
 
 (define (make-sound)
   #t)
+  
+(define (play-drums f)
+  (if (null? f)
+      (play ding)
+      (cond ((eq? f 'kick-drum) (play (get-drum-samples "sample1.wav")))
+            ((eq? f 'hi-top) (play (get-drum-samples "sample2.wav")))
+            ((eq? f 'snare) (play (get-drum-samples "sample3.wav"))
+            ((eq? f 'nothing) (play ding)))))
+  
+(define (get-samples sound)
+  (rs-read sound))
 
-;; Tyler todo
