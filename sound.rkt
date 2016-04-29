dfe65175805bb03a2434d030ec5ed70c292db931
tylerbezuka <Tyler_Bezuka@student.uml.edu>
2016-04-29 09:07:47 -0400

    Comments added for function explanantions (rsound)

--- sound.rkt
+++ sound.rkt
@@ -11,12 +11,16 @@
 (provide repeat-sound)
 (provide record)
 
+;;list of valid strings that can be accepted and passed from parser
+;;if an obect is passed that does not match one of these strings (play ding) is executed in its place. 
 (define valid-list
   (list "drum1" "drum2" "drum3" "drum4"
         "synth1" "synth2" "synth3" "synth4"
         "clap1" "clap2" "clap3" "claves"))
 
 ;;depending on input entered from user in text editor a single sound will be played once. 
+;;to give multiple sounds a more fluid sounding sequence sleep was used. 
+;;Without sleep all sounds are played in at once, defeating the purpose of entering multiple sounds. Sleep solves this problem.
 (define (single-sound sound)
   (if (null? sound)
       (play ding)
@@ -33,8 +37,9 @@
             ((eq? sound 'clap3) (begin (play (rs-read "SoundSamples/Clap-3.wav")) (sleep .25)))
             ((eq? sound 'claves) (begin (play (rs-read "SoundSamples/Claves.wav")) (sleep .25))))))
 
-;;NOT FINISHED
-;;Function repeats the selected sound for however many times the user wishes. 
+
+;;Function repeats the selected sound for however many times the user wishes using rs-append and recursion to combine 
+;;sounds
 (define (repeat-sound sound repeat)
   (if (< repeat 1)
       (single-sound sound)
